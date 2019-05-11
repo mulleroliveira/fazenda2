310d594e965238d1db990c1f687ea835423204f8
Aluno <you@example.com>
2019-05-10 21:39:46 -0300

    show!

---
+++ gestao/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Creator(models.Model):
+    name = models.CharField(max_length=20)
+    cpf = models.CharField(max_length=11, unique=True)
+    date_birth = models.DateField()
+
+class Farm(models.Model):
+    name = models.CharField(max_length=20)
+    address = models.CharField(max_length=20)
+    creator = models.ForeignKey(Creator, on_delete=models.CASCADE)
+
+class Animal(models.Model):
+
+    tipagem_choices = (
+        ("A", "Abate"),
+        ("M", "Matriz")
+    )
+
+    sexo_choices = (
+        ("M", "Macho"),
+        ("F", "Fêmea")
+    )
+
+    name = models.CharField(max_length=20)
+    sexo = models.CharField(max_length=1, choices=sexo_choices)
+    tipagem = models.CharField(max_length=1, choices=tipagem_choices)
+    fazenda = models.ForeignKey(Farm, on_delete=models.CASCADE)
