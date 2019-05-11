0d52a56ff53c8bca0aed2766f5cadb5a87461bc7
Bruno <you@example.com>
2019-05-11 20:02:21 -0300

    ...

--- gestao/views.py
+++ gestao/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from . models import Animal, Creator, Farm
 from . forms import FormAnimal, FormCreator, FormFarm
+from django.http import HttpResponse
+
 
 def home(request):
     return render(request, 'home/home.html')
