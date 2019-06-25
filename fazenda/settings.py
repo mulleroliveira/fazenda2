2bbdb6a8f8c1ae29d789e53ee3a64f88e39e124f
Muller <you@example.com>
2019-06-24 23:42:49 -0300

    Implementação do 'Esqueci a senha?'

--- fazenda/settings.py
+++ fazenda/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL="/gestao/"
 LOGOUT_REDIRECT_URL="/gestao/"
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
